25495ad5aee58bd3d62ef3e5a35fbd78452b5e73
Ty the Smonk <pnkbuzz877@gmail.com>
2024-03-21 04:02:31 -0400

    DESIGNER AAAAAAAAAAAAAAA

--- Stock Tracker/Register.Designer.cs
+++ Stock Tracker/Register.Designer.cs
@@ -43,9 +43,10 @@
             userGuideToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             fAQToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             contactUsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            websiteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             documentationToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             toolTip1 = new System.Windows.Forms.ToolTip(components);
-            websiteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            lblInstruct = new System.Windows.Forms.Label();
             menuStrip1.SuspendLayout();
             SuspendLayout();
             // 
@@ -66,7 +67,7 @@
             // 
             lblEmail.AutoSize = true;
             lblEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0);
-            lblEmail.Location = new System.Drawing.Point(125, 68);
+            lblEmail.Location = new System.Drawing.Point(125, 35);
             lblEmail.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             lblEmail.Name = "lblEmail";
             lblEmail.Size = new System.Drawing.Size(71, 25);
@@ -77,7 +78,7 @@
             // 
             lblRepeatPassword.AutoSize = true;
             lblRepeatPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0);
-            lblRepeatPassword.Location = new System.Drawing.Point(9, 133);
+            lblRepeatPassword.Location = new System.Drawing.Point(9, 100);
             lblRepeatPassword.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             lblRepeatPassword.Name = "lblRepeatPassword";
             lblRepeatPassword.Size = new System.Drawing.Size(187, 25);
@@ -88,7 +89,7 @@
             // 
             lblPassword.AutoSize = true;
             lblPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0);
-            lblPassword.Location = new System.Drawing.Point(84, 96);
+            lblPassword.Location = new System.Drawing.Point(84, 63);
             lblPassword.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             lblPassword.Name = "lblPassword";
             lblPassword.Size = new System.Drawing.Size(112, 25);
@@ -97,7 +98,7 @@
             // 
             // txtEmail
             // 
-            txtEmail.Location = new System.Drawing.Point(215, 68);
+            txtEmail.Location = new System.Drawing.Point(215, 35);
             txtEmail.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             txtEmail.Name = "txtEmail";
             txtEmail.Size = new System.Drawing.Size(116, 23);
@@ -106,7 +107,7 @@
             // 
             // txtVerify
             // 
-            txtVerify.Location = new System.Drawing.Point(215, 138);
+            txtVerify.Location = new System.Drawing.Point(215, 105);
             txtVerify.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             txtVerify.Name = "txtVerify";
             txtVerify.PasswordChar = '*';
@@ -116,7 +117,7 @@
             // 
             // txtPassword
             // 
-            txtPassword.Location = new System.Drawing.Point(215, 101);
+            txtPassword.Location = new System.Drawing.Point(215, 68);
             txtPassword.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             txtPassword.Name = "txtPassword";
             txtPassword.PasswordChar = '*';
@@ -157,48 +158,60 @@
             // 
             userToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] { userGuideToolStripMenuItem, fAQToolStripMenuItem, contactUsToolStripMenuItem, websiteToolStripMenuItem });
             userToolStripMenuItem.Name = "userToolStripMenuItem";
-            userToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            userToolStripMenuItem.Size = new System.Drawing.Size(157, 22);
             userToolStripMenuItem.Text = "Help";
             // 
             // userGuideToolStripMenuItem
             // 
             userGuideToolStripMenuItem.Name = "userGuideToolStripMenuItem";
-            userGuideToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            userGuideToolStripMenuItem.Size = new System.Drawing.Size(135, 22);
             userGuideToolStripMenuItem.Text = "User Guide";
             // 
             // fAQToolStripMenuItem
             // 
             fAQToolStripMenuItem.Name = "fAQToolStripMenuItem";
-            fAQToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            fAQToolStripMenuItem.Size = new System.Drawing.Size(135, 22);
             fAQToolStripMenuItem.Text = "FAQ";
             fAQToolStripMenuItem.Click += fAQToolStripMenuItem_Click;
             // 
             // contactUsToolStripMenuItem
             // 
             contactUsToolStripMenuItem.Name = "contactUsToolStripMenuItem";
-            contactUsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            contactUsToolStripMenuItem.Size = new System.Drawing.Size(135, 22);
             contactUsToolStripMenuItem.Text = "Contact Us!";
             contactUsToolStripMenuItem.Click += contactUsToolStripMenuItem_Click;
-            // 
-            // documentationToolStripMenuItem
-            // 
-            documentationToolStripMenuItem.Name = "documentationToolStripMenuItem";
-            documentationToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            documentationToolStripMenuItem.Text = "Documentation";
-            documentationToolStripMenuItem.Click += documentationToolStripMenuItem_Click;
             // 
             // websiteToolStripMenuItem
             // 
             websiteToolStripMenuItem.Name = "websiteToolStripMenuItem";
-            websiteToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            websiteToolStripMenuItem.Size = new System.Drawing.Size(135, 22);
             websiteToolStripMenuItem.Text = "Website";
             websiteToolStripMenuItem.Click += websiteToolStripMenuItem_Click;
+            // 
+            // documentationToolStripMenuItem
+            // 
+            documentationToolStripMenuItem.Name = "documentationToolStripMenuItem";
+            documentationToolStripMenuItem.Size = new System.Drawing.Size(157, 22);
+            documentationToolStripMenuItem.Text = "Documentation";
+            documentationToolStripMenuItem.Click += documentationToolStripMenuItem_Click;
+            // 
+            // lblInstruct
+            // 
+            lblInstruct.AutoSize = true;
+            lblInstruct.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0);
+            lblInstruct.Location = new System.Drawing.Point(9, 128);
+            lblInstruct.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            lblInstruct.Name = "lblInstruct";
+            lblInstruct.Size = new System.Drawing.Size(259, 75);
+            lblInstruct.TabIndex = 11;
+            lblInstruct.Text = "Password MUST Contain:\r\nAt least 7 characters AND\r\nAt least 1 Number";
             // 
             // frmRegister
             // 
             AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             ClientSize = new System.Drawing.Size(380, 255);
+            Controls.Add(lblInstruct);
             Controls.Add(menuStrip1);
             Controls.Add(btnBack);
             Controls.Add(txtPassword);
@@ -238,5 +251,6 @@
         private System.Windows.Forms.ToolStripMenuItem documentationToolStripMenuItem;
         private System.Windows.Forms.ToolTip toolTip1;
         private System.Windows.Forms.ToolStripMenuItem websiteToolStripMenuItem;
+        private System.Windows.Forms.Label lblInstruct;
     }
 }
